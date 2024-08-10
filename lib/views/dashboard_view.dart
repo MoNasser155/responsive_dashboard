975c33902270c2ae82bfe4985b4f4c0d59f6e0ba
Mo_Nasser <100493763+MoNasser155@users.noreply.github.com>
2024-08-11 02:43:00 +0300

    initialize app and make everything redy

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/widgets/dashboard_desktop_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+        mobileLayout: (context) => const SizedBox(width: 600),
+        tabletLayout: (context) => const SizedBox(width: 600),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
+  }
+}
